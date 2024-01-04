8d54076703f61abaef2e1bdae392bb7c8ca26a6d
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-04 17:36:16 +0100

    Finished header and added Magic Motion

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,24 @@
-
+import logo from '/traduccion.png'
+import { IconBrandGithub, IconBrandLinkedin } from '@tabler/icons-react'
 
 const Header = () => {
-    return (
-        <header>
+  return (
+    <header>
+      <div className='left-part'>
+        <img src={logo} alt='Logo' />
+        <h1>Fast translation</h1>
+      </div>
 
-        </header>
-    )
+      <div className='right-part'>
+        <a href='https://github.com/forjoa/fast-translation' target='_blank'>
+          <IconBrandGithub size={35} />
+        </a>
+        <a href='https://www.linkedin.com/in/joaquin-trujillo-851547254/' target='_blank'>
+          <IconBrandLinkedin size={35} />
+        </a>
+      </div>
+    </header>
+  )
 }
 
 export default Header
