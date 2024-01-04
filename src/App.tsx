e713926bfdaa494bdcd84c9ae7680396773f3621
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-04 16:52:58 +0100

    Created and added Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import TranslationApp from "./components/TranslationApp.tsx"
+import TranslationApp from './components/TranslationApp.tsx'
+import Header from './components/Header.tsx'
 
 function App() {
   return (
     <>
+      <Header />
       <TranslationApp />
     </>
   )
