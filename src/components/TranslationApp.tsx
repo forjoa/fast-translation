047c37034c2bb13b6ccbdc77289c6115cfce8c8a
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-01-04 16:49:45 +0100

    Added magic motion component

--- src/components/TranslationApp.tsx
+++ src/components/TranslationApp.tsx
@@ -3,6 +3,8 @@ import languages from '../utils/languages.ts'
 
 import { useState } from 'react'
 
+import { MagicMotion } from 'react-magic-motion'
+
 const TranslationApp = () => {
   const [textToTranslate, setTextToTranslate] = useState('')
   const [targetLanguage, setTargetLanguage] = useState('')
@@ -18,39 +20,41 @@ const TranslationApp = () => {
   }
 
   return (
-    <div className='container'>
-      <h2>Text to translate</h2>
-      <textarea
-        value={textToTranslate}
-        onChange={(e) => setTextToTranslate(e.target.value)}
-        placeholder='Enter text to translate...'
-        className='textarea'
-      />
-      <div className='language-select'>
-        <select
-          value={targetLanguage}
-          onChange={(e) => setTargetLanguage(e.target.value)}
-          className='select'
-        >
-          {languages.map((language) => (
-            <option key={language.code} value={language.code}>
-              {language.name}
-            </option>
-          ))}
-        </select>
+    <MagicMotion>
+      <div className='container'>
+        <h2>Text to translate</h2>
+        <textarea
+          value={textToTranslate}
+          onChange={(e) => setTextToTranslate(e.target.value)}
+          placeholder='Enter text to translate...'
+          className='textarea'
+        />
+        <div className='language-select'>
+          <select
+            value={targetLanguage}
+            onChange={(e) => setTargetLanguage(e.target.value)}
+            className='select'
+          >
+            {languages.map((language) => (
+              <option key={language.code} value={language.code}>
+                {language.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <button onClick={handleTranslation} className='button'>
+          Translate
+        </button>
+
+        {translatedText && (
+          <>
+            <h2>Text translated</h2>
+            <textarea className='textarea' value={translatedText} />
+          </>
+        )}
       </div>
-
-      <button onClick={handleTranslation} className='button'>
-        Translate
-      </button>
-
-      {translatedText && (
-        <>
-        <h2>Text translated</h2>
-        <textarea className='textarea' value={translatedText} />
-        </>
-      )}
-    </div>
+    </MagicMotion>
   )
 }
 
